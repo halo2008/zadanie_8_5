c361812358599cc4399872787176eb4b0bde35c0
halo2008 <ksedkowski@gmail.com>
2017-04-25 19:18:36 +0200

    final version

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('a'),
+    h = prompt('h');
+var triangleArea = (a*h)/2;
+alert('pole trojkata o podstawie a, ' + triangleArea);
+console.log('Pole trojkata o podstawie a: ' + a + ' i wysokosci h: ' + h + ' wynosi: ' + triangleArea);
